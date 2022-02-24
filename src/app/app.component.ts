cfcb0ace6a8e329a94ff33b22b0d2f7280f1729c
Yatoo2018 <tanpf2018@163.com>
2022-02-24 13:05:28 +0800

    normal组件的生命周期

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,15 +17,6 @@ import {
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent
-  implements
-    OnChanges,
-    OnInit,
-    DoCheck,
-    AfterContentInit,
-    AfterContentChecked,
-    AfterViewInit,
-    AfterViewChecked,
-    OnDestroy
 {
   name = 'Angular ' + VERSION.major;
   messages = [];
@@ -37,62 +28,6 @@ export class AppComponent
     this.subject.next({
       type: 'constructor exec',
       content: 'AppComponent class instance',
-    });
-  }
-
-  ngOnChanges() {
-    this.subject.next({
-      type: 'lifecycle',
-      content: 'AppComponent ngOnChanges',
-    });
-  }
-
-  ngOnInit() {
-    this.subject.next({
-      type: 'lifecycle',
-      content: 'AppComponent ngOnInit',
-    });
-  }
-
-  ngDoCheck() {
-    this.subject.next({
-      type: 'lifecycle',
-      content: 'AppComponent ngDoCheck',
-    });
-  }
-
-  ngAfterContentInit() {
-    this.subject.next({
-      type: 'lifecycle',
-      content: 'AppComponent ngAfterContentInit',
-    });
-  }
-
-  ngAfterContentChecked() {
-    this.subject.next({
-      type: 'lifecycle',
-      content: 'AppComponent ngAfterContentChecked',
-    });
-  }
-
-  ngAfterViewInit() {
-    this.subject.next({
-      type: 'lifecycle',
-      content: 'AppComponent ngAfterViewInit',
-    });
-  }
-
-  ngAfterViewChecked() {
-    this.subject.next({
-      type: 'lifecycle',
-      content: 'AppComponent ngAfterViewChecked',
-    });
-  }
-
-  ngOnDestroy() {
-    this.subject.next({
-      type: 'lifecycle',
-      content: 'AppComponent ngOnDestroy',
     });
   }
 }
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,10 +1,94 @@
-import { Component, Input } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'hello',
   template: `<h1>Hello, {{name}}!</h1>`,
   styles: [`h1 { font-family: Lato; }`],
 })
-export class HelloComponent {
+export class HelloComponent
+  implements
+    OnChanges,
+    OnInit,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
   @Input() name;
+  subject = window['subject'];
+  constructor() {
+    this.subject.next({
+      type: 'constructor exec',
+      content: 'class instance',
+    });
+  }
+
+  ngOnChanges() {
+    this.subject.next({
+      type: 'lifecycle',
+      content: 'ngOnChanges',
+    });
+  }
+
+  ngOnInit() {
+    this.subject.next({
+      type: 'lifecycle',
+      content: 'ngOnInit',
+    });
+  }
+
+  ngDoCheck() {
+    this.subject.next({
+      type: 'lifecycle',
+      content: 'ngDoCheck',
+    });
+  }
+
+  ngAfterContentInit() {
+    this.subject.next({
+      type: 'lifecycle',
+      content: 'ngAfterContentInit',
+    });
+  }
+
+  ngAfterContentChecked() {
+    this.subject.next({
+      type: 'lifecycle',
+      content: 'ngAfterContentChecked',
+    });
+  }
+
+  ngAfterViewInit() {
+    this.subject.next({
+      type: 'lifecycle',
+      content: 'ngAfterViewInit',
+    });
+  }
+
+  ngAfterViewChecked() {
+    this.subject.next({
+      type: 'lifecycle',
+      content: 'ngAfterViewChecked',
+    });
+  }
+
+  ngOnDestroy() {
+    this.subject.next({
+      type: 'lifecycle',
+      content: 'ngOnDestroy',
+    });
+  }
 }
